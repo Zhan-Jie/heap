d99c4a6be0734cb9ef9d665d27fcb9e1a46b28ae
zhanj <916952976@qq.com>
2018-04-28 23:20:11 +0800

    add element to heap

--- src/heap/heap.go
+++ src/heap/heap.go
@@ -2,4 +2,35 @@ package heap
 
 // Heap sss
 type Heap struct {
+	elements []Comparable
+}
+
+// Comparable Element
+type Comparable interface {
+	GreaterThan(*Comparable) bool
+}
+
+// Add function
+func (heap *Heap) Add(element Comparable) {
+	heap.elements = append(heap.elements, element)
+	pos := len(heap.elements) - 1
+
+	for {
+		if pos == 0 {
+			return
+		}
+		var parent int
+		if pos%2 == 0 { // right child
+			parent = (pos - 2) / 2
+		} else { // left child
+			parent = (pos - 1) / 2
+		}
+		newEle := heap.elements[pos]
+		if !newEle.GreaterThan(&heap.elements[parent]) {
+			break
+		}
+		heap.elements[pos] = heap.elements[parent]
+		heap.elements[parent] = newEle
+		pos = parent
+	}
 }
--- test.go
+++ test.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+
+	heap "./src/heap"
 )
 
+// MyInt wrapper
+type MyInt int
+
+// GreaterThan implements interface
+func (i MyInt) GreaterThan(b *heap.Comparable) bool {
+	bInt, ok := (*b).(MyInt)
+	if ok {
+		return i > MyInt(bInt)
+	}
+	panic(fmt.Sprintf("%v is not a int\n", *b))
+}
+
 func main() {
-	fmt.Println("hello, sb~")
+	h := heap.Heap{}
+	h.Add(MyInt(1))
+	h.Add(MyInt(2))
+	h.Add(MyInt(3))
+	h.Add(MyInt(4))
+	h.Add(MyInt(5))
+	h.Add(MyInt(8))
+	h.Add(MyInt(4))
+	fmt.Printf("hello, sb~, %v\n", h)
 }
